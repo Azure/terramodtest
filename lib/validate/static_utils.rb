4ae3539a3a2e6c5ae9018567ab1b8e1924b3bea1
yupwei <yupwei@microsoft.com>
2020-03-18 16:48:09 +0800

    add terraform init

--- lib/validate/static_utils.rb
+++ lib/validate/static_utils.rb
@@ -11,6 +11,10 @@ def lint_tf
   if ENV['TERRAFORM_VERSION'].nil? || ENV['TERRAFORM_VERSION'].start_with?("0.11.")
     message = `terraform validate -check-variables=false 2>&1`
   elsif ENV['TERRAFORM_VERSION'].start_with?("0.12.")
+    success = system ("terraform init")
+        if not success
+          raise "ERROR: terraform init failed!\n".red
+        end
     message = `terraform validate >/dev/null`
   end
 
